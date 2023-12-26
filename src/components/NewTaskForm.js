53f99a9d821146bba09247d23343e34df21d7304
Andrei Miculita <andreimiculita@gmail.com>
2023-12-26 01:15:23 +0100

    Make rest of page dark when adding new task

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -5,18 +5,18 @@ const FIELDS = [
         type: 'number',
         min: 1,
         max: 4,
-        placeholder: 'Task priority (1-4) (Optional)',
+        placeholder: 'Priority (1-4)',
         state: 'priority',
     },
     {
         type: 'datetime-local',
-        placeholder: 'Planned at (Optional)',
+        placeholder: 'Planned at',
         state: 'plannedAt',
     },
     {
         type: 'number',
         min: 1,
-        placeholder: 'Duration (minutes) (Optional)',
+        placeholder: 'Duration (mins)',
         state: 'duration',
     },
 ];
@@ -26,6 +26,16 @@ function NewTaskForm({ onCreate }) {
     const [priority, setPriority] = useState('');
     const [plannedAt, setPlannedAt] = useState('');
     const [duration, setDuration] = useState('');
+    const [isFocused, setIsFocused] = useState(false);
+
+
+    const handleFocus = () => {
+        (!isFocused) && setIsFocused(true);
+    };
+
+    const handleBlur = () => {
+        (isFocused) && setIsFocused(false);
+    };
 
     const stateSetters = {
         priority: setPriority,
@@ -43,29 +53,36 @@ function NewTaskForm({ onCreate }) {
     };
 
     return (
-        <form onSubmit={handleSubmit} className="newTaskForm">
-            <input
-                type="text"
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-                placeholder="New task title"
-                required
-            />
-            <div className="taskDetails">
-                {FIELDS.map(({ type, min, max, placeholder, state }) => (
-                    <input
-                        key={state}
-                        type={type}
-                        min={min}
-                        max={max}
-                        value={{ priority, plannedAt, duration }[state]}
-                        onChange={(e) => stateSetters[state](e.target.value)}
-                        placeholder={placeholder}
-                    />
-                ))}
-            </div>
-            <button type="submit">Add Task</button>
-        </form>
+        <div>
+            {isFocused && <div className="overlay"></div>}
+            <form className="newTaskForm" onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                    onFocus={handleFocus}
+                    onBlur={handleBlur}
+                    placeholder="New task title"
+                    required
+                />
+                <div className="taskDetails">
+                    {FIELDS.map(({ type, min, max, placeholder, state }) => (
+                        <input
+                            key={state}
+                            type={type}
+                            min={min}
+                            max={max}
+                            value={{ priority, plannedAt, duration }[state]}
+                            onChange={(e) => stateSetters[state](e.target.value)}
+                            onFocus={handleFocus}
+                            onBlur={handleBlur}
+                            placeholder={placeholder}
+                        />
+                    ))}
+                </div>
+                <button type="submit">Add Task</button>
+            </form>
+        </div>
     );
 }
 
