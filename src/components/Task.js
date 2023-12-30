bcf606f67ed1daf65e25a30ee4980ce2aa5aa9d9
Andrei Miculita <andreimiculita@gmail.com>
2023-12-30 19:52:28 +0100

    Change prio color calculation method

--- src/components/Task.js
+++ src/components/Task.js
@@ -4,9 +4,19 @@ import { Draggable } from 'react-beautiful-dnd';
 import TaskLinkButton from './TaskLinkButton';
 import TaskAttributes from './TaskAttributes';
 
+function getPriorityColor(priority) {
+    const maxPriority = 4; // replace with your maximum priority
+    const minPriority = 1; // replace with your minimum priority
+  
+    const ratio = (priority - minPriority) / (maxPriority - minPriority);
+  
+    const hue = ratio * 120; // hue goes from 0 (red) to 120 (green)
+  
+    return `hsl(${hue}, 100%, 50%)`;
+  }
+
 function Task({ task }) {
-    const maxPriority = 4;
-    const priorityColor = `hsl(${task.priority * 360 / maxPriority}, 100%, 50%)`;
+    const priorityColor = getPriorityColor(task.priority);
 
     return (
         <Draggable draggableId={task.id.toString()} index={task.priority - 1}>
