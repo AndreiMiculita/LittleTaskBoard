339e0a4633add8225b0ead7c9a07e9d0d3695fa9
Andrei Miculita <andreimiculita@gmail.com>
2024-01-01 21:28:25 +0100

    Remove multiple boards, leave just one

--- api/flaskr/sidebar.py
+++ api/flaskr/sidebar.py
@@ -15,19 +15,9 @@ def get_sidebar():
                 'links': [
                     {
                         'id': 1,
-                        'title': 'Board 1',
-                        'url': '/board/1'
-                    },
-                    {
-                        'id': 2,
-                        'title': 'Board 2',
-                        'url': '/board/2'
-                    },
-                    {
-                        'id': 3,
-                        'title': 'Board 3',
-                        'url': '/board/3'
-                    },
+                        'title': 'Board',
+                        'url': '/'
+                    }
                 ]
             },
             {
