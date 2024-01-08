fcd405776fb139e44ba79d1bd1542f40fe7093c4
Andrei Miculita <andreimiculita@gmail.com>
2024-01-08 23:46:59 +0100

    Add task types icons and handle missing status

--- src/components/TaskRow.js
+++ src/components/TaskRow.js
@@ -2,12 +2,18 @@ import React from 'react';
 import Planning from './Planning';
 import { useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBrain } from '@fortawesome/free-solid-svg-icons';
+import { faBrain, faUsers, faNoteSticky } from '@fortawesome/free-solid-svg-icons';
 
 const STATUS_MAP = {
     1: 'To Do',
     2: 'In Progress',
     3: 'Done'
+};
+
+const taskTypes = {
+    '0': <FontAwesomeIcon icon={faNoteSticky} />,
+    '1': <FontAwesomeIcon icon={faBrain} />,
+    '2': <FontAwesomeIcon icon={faUsers} />
 };
 
 const TaskRow = ({ task }) => {
@@ -23,8 +29,8 @@ const TaskRow = ({ task }) => {
             <td>{task.priority ? task.priority : "No Priority"}</td>
             <td>{task.planned_at ? <Planning plannedAt={task.planned_at} duration={task.duration} /> : "Not Planned"}</td>
             <td>{task.duration ? task.duration : "No Duration"}</td>
-            <td>{task.type === 1 ? <FontAwesomeIcon icon={faBrain} /> : null}</td>
-            <td>{STATUS_MAP[task.status]}</td>
+            <td>{taskTypes[task.task_type]}</td>
+            <td>{STATUS_MAP[task.status] ? STATUS_MAP[task.status] : "No Status"}</td>
         </tr>
     );
 };
