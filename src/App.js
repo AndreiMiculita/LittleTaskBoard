1d2cdcdbc060c2566628b11ae9b8bbb30c7d9721
Andrei Miculita <andreimiculita@gmail.com>
2024-01-21 22:07:25 +0100

    Reindent App.js

--- src/App.js
+++ src/App.js
@@ -14,60 +14,60 @@ import PageLayout from './layouts/PageLayout.tsx';
 import RegisterPage from './pages/RegisterPage';
 
 function LogoutHandler({ setAuth }) {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  useEffect(() => {
-    if (window.location.pathname === '/logout') {
-      const authService = new AuthService();
-      authService.logout();
-      setAuth(authService); // This will cause a re-render, which we want, to hide other elements in PageLayout
-      toast("You have been logged out.");
-      navigate('/login')
-    }
-  }, [navigate]);
+    useEffect(() => {
+        if (window.location.pathname === '/logout') {
+            const authService = new AuthService();
+            authService.logout();
+            setAuth(authService); // This will cause a re-render, which we want, to hide other elements in PageLayout
+            toast("You have been logged out.");
+            navigate('/login')
+        }
+    }, [navigate]);
 
-  return null;
+    return null;
 }
 
 function App() {
-  const [auth, setAuth] = useState(new AuthService());
+    const [auth, setAuth] = useState(new AuthService());
 
-  return (
-    <Router>
-      <PageLayout auth={auth}>
-        <Routes>
-          <Route path="/login" element={<LoginPage setAuth={setAuth}/>} />
-          <Route path="/register" element={<RegisterPage setAuth={setAuth}/>} />
-          <Route path="/" element={
-            <PrivateRoute>
-              <BoardPage auth={auth} />
-            </PrivateRoute>
-          } />
-          <Route path="/tasks" element={
-            <PrivateRoute>
-              <TasksPage auth={auth} />
-            </PrivateRoute>
-          } />
-          <Route path="/tasks/:id" element={
-            <PrivateRoute>
-              <TaskDetailPage auth={auth} />
-            </PrivateRoute>
-          } />
-          <Route path="/calendar" element={
-            <PrivateRoute>
-              <CalendarPage auth={auth} />
-            </PrivateRoute>
-          } />
-          <Route path="/insights" element={
-            <PrivateRoute>
-              <InsightsPage auth={auth} />
-            </PrivateRoute>
-          } />
-        </Routes>
-        <LogoutHandler setAuth={setAuth} />
-      </PageLayout>
-    </Router>
-  );
+    return (
+        <Router>
+            <PageLayout auth={auth}>
+                <Routes>
+                    <Route path="/login" element={<LoginPage setAuth={setAuth} />} />
+                    <Route path="/register" element={<RegisterPage setAuth={setAuth} />} />
+                    <Route path="/" element={
+                        <PrivateRoute>
+                            <BoardPage auth={auth} />
+                        </PrivateRoute>
+                    } />
+                    <Route path="/tasks" element={
+                        <PrivateRoute>
+                            <TasksPage auth={auth} />
+                        </PrivateRoute>
+                    } />
+                    <Route path="/tasks/:id" element={
+                        <PrivateRoute>
+                            <TaskDetailPage auth={auth} />
+                        </PrivateRoute>
+                    } />
+                    <Route path="/calendar" element={
+                        <PrivateRoute>
+                            <CalendarPage auth={auth} />
+                        </PrivateRoute>
+                    } />
+                    <Route path="/insights" element={
+                        <PrivateRoute>
+                            <InsightsPage auth={auth} />
+                        </PrivateRoute>
+                    } />
+                </Routes>
+                <LogoutHandler setAuth={setAuth} />
+            </PageLayout>
+        </Router>
+    );
 }
 
 export default App;
