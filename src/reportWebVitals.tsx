d46a640e1b1626c351626eefc1ae90032e2a5e82
Andrei Miculita <andreimiculita@gmail.com>
2024-01-23 21:23:02 +0100

    Convert setupTests and reportWebVitals to tsx

---
+++ src/reportWebVitals.tsx
@@ -0,0 +1,14 @@
+import { ReportHandler } from 'web-vitals';
+function reportWebVitals(onPerfEntry: ReportHandler) {
+    if (onPerfEntry && typeof onPerfEntry === 'function') {
+        import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+            getCLS(onPerfEntry);
+            getFID(onPerfEntry);
+            getFCP(onPerfEntry);
+            getLCP(onPerfEntry);
+            getTTFB(onPerfEntry);
+        });
+    }
+}
+
+export default reportWebVitals;
