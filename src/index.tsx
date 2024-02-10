83ebb5ab4af4a3e3b2ed618e7961c1c9b27e0002
Andrei Miculita <andreimiculita@gmail.com>
2024-02-10 03:23:59 +0100

    Remove App.css entirely

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './styles/index.css';
-import './styles/App.css';
 import './styles/globals.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
